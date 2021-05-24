63a135c66a62c69a3bc480b17c7a1690f82cda63
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-05-24 19:37:16 +0545

    Introduction

--- src/component/Intro/Intro.jsx
+++ src/component/Intro/Intro.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import '../Intro/intro.css'
 
 const Intro = () => {
     return (
-        <div>
-            intro
+        <div className="intro-content">
+            <div className="flex-row intro-content">
+                <div className="flex-1">
+                    <div className="my-pic">
+                            <img src="assets/img/my-pic.jpg" alt="Rakesh Shrestha" width="100%" height="100%" />
+                    </div>
+                </div>
+
+                <div className="flex-1 my-intro">
+                    <div className="intro">
+                        <h3>Hello, Its me</h3>
+                        <h1>RAKESH SHRESTHA</h1>
+                        <h4>I am a <span className="designation">Software Developer</span>.</h4>
+                    </div>
+                </div>
+            </div>
         </div>
+            
     )
 }
 
